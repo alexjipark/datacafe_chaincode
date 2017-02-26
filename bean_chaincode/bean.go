0a116f9dee0248e534c9863d4793cd9425bc0298
alexjipark <alexjipark@gmail.com>
2017-02-26 17:10:46 +0900

    Implented bean.go

--- bean_chaincode/bean.go
+++ bean_chaincode/bean.go
@@ -16,173 +16,120 @@ limitations under the License.
 
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
-
 import (
-	"errors"
-	"fmt"
-	"strconv"
-
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
+	"github.com/op/go-logging"
+	"errors"
+	"strconv"
+	"encoding/binary"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+var beanLogger = logging.MustGetLogger("bean_cc")
+
+type BeanChaincode struct {
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return nil, err
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return nil, err
-	}
-
+func (bc *BeanChaincode) Init(stub shim.ChaincodeStubInterface,
+		function string, args []string) ([]byte, error) {
+	beanLogger.Debug("Entered Init func..")
 	return nil, nil
 }
 
-// Transaction makes payment of X units from A to B
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
+
+func (bc *BeanChaincode) getBeanBalance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	beanLogger.Debug("=============== getBeanBalance ===============")
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expected 1..")
+	}
+	requestAddress := args[0]
+
+	//Get the state from the ledger
+	beanBytes, err := stub.GetState(requestAddress)
+	if err != nil {
+		return []byte(strconv.Itoa(0)), errors.New("No BeanBalance stored in the ledger")
 	}
 
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
+	// binary.BigEndian.Uint64(mySlice)
+	beanLogger.Info("Address[%x]'s Balance : %d", requestAddress, binary.BigEndian.Uint64(beanBytes))
+	return beanBytes, nil
+}
 
+func (bc *BeanChaincode) transferBean(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+
+	var remainBean4Sender, newBean4Receiver int
+
+	beanLogger.Debug("=============== transferBean =================")
 	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 3")
+		return nil, errors.New("Incorrect number of arguments. Expected 3..")
 	}
 
-	A = args[0]
-	B = args[1]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
+	sendAddr := args[0]
+	recvAddr := args[1]
+	beanAmount, err := strconv.ParseInt(args[2], 10, 64)
 	if err != nil {
-		return nil, errors.New("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return nil, errors.New("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return nil, errors.New("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return nil, errors.New("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[2])
-	if err != nil {
-		return nil, errors.New("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return nil, err
+		return nil, errors.New("Error in Coverting beanAmount")
 	}
 
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
+	sendBeanBytes, err := stub.GetState(sendAddr)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Incorrect Address for Sender")
+	}
+	sendBean := binary.BigEndian.Uint64(sendBeanBytes)
+
+	recvBeanBytes, err := stub.GetState(recvAddr)
+	if err != nil {
+		beanLogger.Debug("Address[%x] doesn't have the stored balance..", recvAddr)
+		recvBeanBytes = []byte(strconv.Itoa(0))
+	}
+	recvBean := binary.BigEndian.Uint64(recvBeanBytes)
+
+	if (sendBean < beanAmount) {
+		return nil, errors.New("Not enough for Sender..")
+	}
+	remainBean4Sender = sendBean - beanAmount
+	newBean4Receiver = recvBean + beanAmount
+
+	// Store new Bean Amounts
+	err = stub.PutState(sendAddr, []byte(strconv.Itoa(remainBean4Sender)))
+	if err != nil {
+		return nil, errors.New("Error in putting State with sendAddress")
+	}
+	err = stub.PutState(recvAddr, []byte(strconv.Itoa(newBean4Receiver)))
+	if err != nil {
+		// [AJ] Problem : what if PutState with sendAddr
+		return nil, errors.New("Error in putting State with recvAddress")
 	}
 
 	return nil, nil
 }
 
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+func (bc *BeanChaincode) Invoke(stub shim.ChaincodeStubInterface,
+		function string, args []string) ([]byte, error) {
+	beanLogger.Debug("Entered Invoke func..")
+
+	if function == "transferBean" {
+		return bc.transferBean(stub, args)
 	}
 
-	A := args[0]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
-	if err != nil {
-		return nil, errors.New("Failed to delete state")
-	}
-
-	return nil, nil
+	return nil, errors.New("Received Unknown Function Invokation")
 }
 
-// Query callback representing the query of a chaincode
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if function != "query" {
-		return nil, errors.New("Invalid query function name. Expecting \"query\"")
-	}
-	var A string // Entities
-	var err error
+func (bc *BeanChaincode) Query(stub shim.ChaincodeStubInterface,
+		function string, args []string) ([]byte, error) {
+	beanLogger.Debug("Entered Query func..")
 
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the person to query")
+	if function == "getBeanBalance" {
+		return bc.getBeanBalance(stub, args)
 	}
 
-	A = args[0]
+	return nil, errors.New("Received Unknown Function Query")
 
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return Avalbytes, nil
 }
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(BeanChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting Bean chaincode: %s", err)
 	}
 }

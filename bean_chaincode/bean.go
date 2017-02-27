866aa75e29f5debadc0843b2f95cc5fc0f75c9c5
alexjipark <alexjipark@gmail.com>
2017-02-27 12:26:31 +0900

    minor fix

--- bean_chaincode/bean.go
+++ bean_chaincode/bean.go
@@ -55,7 +55,7 @@ func (bc *BeanChaincode) getBeanBalance(stub shim.ChaincodeStubInterface, args [
 
 func (bc *BeanChaincode) transferBean(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
-	var remainBean4Sender, newBean4Receiver uint64
+	var remainBean4Sender, newBean4Receiver int
 
 //	beanLogger.Debug("=============== transferBean =================")
 	if len(args) != 3 {
